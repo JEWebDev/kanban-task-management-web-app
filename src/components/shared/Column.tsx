3f2db37ea88e335cd9f0152208cc9aa484dd05a1
Juan Esparza <83143208+JEWebDev@users.noreply.github.com>
2026-02-27 06:55:28 -0600

    fix(ui): fix column heading dot to display it's color

--- src/components/shared/Column.tsx
+++ src/components/shared/Column.tsx
@@ -8,7 +8,10 @@ function Column({ title, dotColor, taskCount, children }: ColumnProps) {
   return (
     <section className="h-full w-70 flex flex-col gap-6 shrink-0">
       <h3 className="heading-s text-grey-400 uppercase flex items-center gap-3">
-        <span className={`bg-[${dotColor}] w-3.75 h-3.75 rounded-full`}></span>
+        <span
+          className={` w-3.75 h-3.75 rounded-full`}
+          style={{ backgroundColor: dotColor }}
+        ></span>
         {title} ({taskCount})
       </h3>
       <div className="flex flex-col flex-1 min-h-0 gap-6 overflow-y-auto overflow-x-hidden">
