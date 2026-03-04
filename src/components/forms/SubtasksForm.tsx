b9ea4ecefe96ae6185cfa460b413c26ee4548f0f
Juan Esparza <83143208+JEWebDev@users.noreply.github.com>
2026-03-04 05:33:23 -0600

    feature: implement subtask add and delete functionality
    - user can add, delete and see total subtasks in CreateNewTask modal form.

--- src/components/forms/SubtasksForm.tsx
+++ src/components/forms/SubtasksForm.tsx
@@ -1,25 +1,39 @@
 import SubtaskInput from "../shared/inputs/SubtaskInput";
 import SecondaryButton from "../shared/buttons/SecondaryButton";
+import { useState } from "react";
 
 function SubtasksForm() {
+  const [subtasks, setSubtasks] = useState([
+    crypto.randomUUID(),
+    crypto.randomUUID(),
+  ]);
+
+  const addSubtask = () => {
+    const newSubtasks = [...subtasks, crypto.randomUUID()];
+    setSubtasks(newSubtasks);
+  };
+
+  const deleteSubtask = (id: string) => {
+    const newSubtasks = subtasks.filter((subtask) => subtask !== id);
+    setSubtasks(newSubtasks);
+  };
   return (
     <>
       <fieldset className="overflow-y-auto w-full max-h-29.25 flex flex-col gap-3">
-        <legend className="body-m text-grey-400 mb-2">{`Subtasks`}</legend>
-        <SubtaskInput
-          label="subtask-1"
-          id="subtask-1"
-          onDelete={() => {}}
-          placeholder="e.g. Make coffee"
-        />
-        <SubtaskInput
-          label="subtask-2"
-          id="subtask-2"
-          onDelete={() => {}}
-          placeholder="e.g. Make coffee"
-        />
+        <legend className="body-m text-grey-400 mb-2">{`Subtasks (${subtasks.length})`}</legend>
+        {subtasks.map((subtask, index) => {
+          return (
+            <SubtaskInput
+              key={subtask}
+              label={`subtask-(${index + 1})`}
+              id={subtask}
+              onDelete={() => deleteSubtask(subtask)}
+              placeholder="e.g. Make coffee"
+            />
+          );
+        })}
       </fieldset>
-      <SecondaryButton onClick={() => {}} className="-mt-3">
+      <SecondaryButton onClick={addSubtask} className="-mt-3">
         + Add New Subtask
       </SecondaryButton>
     </>
