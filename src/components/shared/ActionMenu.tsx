de81fb45314e78de288a0d586967c8a431db753d
Juan Esparza <83143208+JEWebDev@users.noreply.github.com>
2026-03-30 14:59:38 -0600

    refactor(ActionMenu): refactor ActionMenu

--- src/components/shared/ActionMenu.tsx
+++ src/components/shared/ActionMenu.tsx
@@ -1,38 +1,41 @@
-import { useState } from "react";
 import IconVerticalElipsis from "../icons/IconVerticalElipsis";
+import { useActionMenu } from "@/hooks/useActionMenu";
+interface ActionMenuProps {
+  className?: string;
+}
 
-function ActionMenu() {
-  const [isOpen, setIsOpen] = useState(false);
-  const openActionMenu = () => {
-    setIsOpen(!isOpen);
+function ActionMenu({ className }: ActionMenuProps) {
+  const { isOpen, menuRef, closeMenu, toggleMenu } = useActionMenu();
+  const handleEdit = () => {
+    closeMenu();
+    console.log("Redirecting to edit modal");
   };
-  const openEditModal = () => {
-    setIsOpen(false);
-    console.log("Opening edit modal");
-  };
-  const openDeleteConfirmationModal = () => {
-    setIsOpen(false);
-    console.log("Opening delete confirmatoin modal");
+  const handleDelete = () => {
+    closeMenu();
+    console.log("Opening confirmation modal");
   };
   return (
     <>
       <button
-        className="w-4 flex justify-end hover:cursor-pointer"
-        onClick={openActionMenu}
+        className="w-10 flex justify-center hover:cursor-pointer"
+        onClick={toggleMenu}
       >
         <IconVerticalElipsis className="w-1 h-4 md:h-5" />
       </button>
       {isOpen && (
-        <div className="p-4 absolute bg-white rounded-lg dark:bg-black-600 flex flex-col gap-4 top-20 right-4">
+        <div
+          className={`p-4 absolute bg-white rounded-lg dark:bg-black-600 flex flex-col gap-4 top-20 right-4 ${className}`}
+          ref={menuRef}
+        >
           <button
             className="w-40 h-5.75 text-left body-l text-grey-400 hover:cursor-pointer"
-            onClick={openEditModal}
+            onClick={handleEdit}
           >
             Edit Task
           </button>
           <button
             className="w-40 h-5.75 body-l text-left text-red-500 hover:cursor-pointer"
-            onClick={openDeleteConfirmationModal}
+            onClick={handleDelete}
           >
             Delete Task
           </button>
